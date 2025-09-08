3f9cc4879be00f4c60fe98ad951eed2769797fc4
D1pper22D1p <161803005+D1pper22D1p@users.noreply.github.com>
2025-09-08 11:05:26 +0000

    da

--- fSharpPractice/Program.fs
+++ fSharpPractice/Program.fs
@@ -1,8 +1,18 @@
 ﻿open System
 
-[<EntryPoint>]
-let main argv =
-    let arr = [| 1..100 |]
-    let Sum = Array.sum (arr)
-    printfn "%d" Sum
-    0
+let employees = dict [
+    ("employee1", 100)
+    ("employee2", 200)
+    ("employee3", 300)
+    ("employee4", 400)
+    ("employee5", 500)
+    ("employee6", 600)
+    ("employee7", 700)
+]
+
+let increaseSalary salary = int (float salary * 1.1)
+
+let updateEmployees = 
+    employees
+    |> Seq.map (fun kvp -> (kvp.Key, increaseSalary kvp.Value))
+    |> type
